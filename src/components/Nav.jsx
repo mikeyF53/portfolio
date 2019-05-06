f673cbd8dd5f319ed62a17fd53bdaf7f96028d51
MikeyF53 <mikefang530@gmail.com>
2019-05-06 15:37:55 -0400

    updated nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,7 +8,7 @@ const Nav = (props) => {
   const { goTo } = props
    return (
     <div className='nav'>
-      <a onClick={() => goTo('main')}><img className='logo' src='#'/></a>
+      {/* <a onClick={() => goTo('main')}><img className='logo' src='#'/></a> */}
       <a onClick={() => goTo('about')}>About</a>
       <a onClick={() => goTo('works')}>Works</a>
       <a onClick={() => goTo('contact')}>Contact</a>
